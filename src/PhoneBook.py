839159b2eded4b457361cfe976c86896785f16b4
Angel Vargas <angelvargas@outlook.es>
2019-09-25 04:15:29 +0100

    Added new functionality to PhoneBook class, including test cases. - can create a new entry using the PhoneBook class - can list entries using the PhoneBook class - can delete entry from the PhoneBook class - can update an existing entry using the PhoneBook class

--- src/PhoneBook.py
+++ src/PhoneBook.py
@@ -1,4 +1,5 @@
 from src.DB import DB
+from src.Contact import ContactEntry
 
 
 class PhoneBook:
@@ -7,9 +8,95 @@ class PhoneBook:
 
     Describe and controls behaviour of the operations in the phone book.
     """
-    def __init__(self):
+    db = None
+
+    def __init__(self, db_path='test_database.sqlite3'):
         """"""
-        database = DB()
-        self.db = database.connect()
+        database = DB(db_path).connect()
+        self.db = database
+
+    def create(self, contact_entry: ContactEntry) -> int:
+        """
+        Create an entry in the Phone Book
+        :param contact_entry:
+        :return: db index
+        """
+        query = ('''INSERT INTO phone_book
+        (surname, firstname, phone_number, address)
+        VALUES (?, ?, ?, ?)''')
+
+        value = (
+            contact_entry.surname,
+            contact_entry.firstname,
+            contact_entry.phone_number,
+            contact_entry.address
+        )
+
+        result = self.db.cursor().execute(query, value)
+        self.db.commit()
+        return result.lastrowid
+
+    def find(self, _id) -> dict:
+        """
+        Find an entry in the Phone Book based on their id
+        :param _id:
+        :return:
+        """
+        query = ('''SELECT * FROM phone_book
+        WHERE id=?''')
+
+        value = (
+            str(_id)
+        )
+
+        result = self.db.cursor().execute(query, value)
+        return result.fetchone()
+
+    def update(self, **kwargs) -> dict:
+        """
+        Update phone book entry
+        :param kwargs:
+        :return: dict
+        """
+        cols = ""
+        idx = kwargs.pop('idx')
+        values = ()
+        for key, value in kwargs.items():
+            if key == 'idx':
+                continue
+            cols += key+"=? "
+            values = values + (value,)
+
+        values = values + (str(idx),)
+
+        query = "UPDATE phone_book SET " + cols + " WHERE id=?"
+
+        self.db.cursor().execute(query, values)
+        row_count = self.db.cursor().rowcount
+
+        return row_count
+
+    def delete(self, _id) -> dict:
+        """
+        Find an entry in the Phone Book based on their id and delete it
+        :param _id:
+        :return:
+        """
+        query = ('''DELETE FROM phone_book
+        WHERE id=?''')
+
+        value = (
+            str(_id)
+        )
+
+        result = self.db.cursor().execute(query, value)
+        self.db.commit()
+        return result.rowcount
+
+
+
+
+
+
 
 
--- tests/unit/test_phone_book.py
+++ tests/unit/test_phone_book.py
@@ -1,23 +1,53 @@
 from src.PhoneBook import PhoneBook
 from src.Contact import ContactEntry
+from src.DB import DB
 import pytest
 
 
+@pytest.fixture
+def database_path():
+    """
+    path to database file location
+    :return: None
+    """
+    return "test_database.sqlite3"
+
+
 @pytest.fixture()
-def phone_book():
-    return PhoneBook()
+def phone_book(database_path):
+    """"""
+    return PhoneBook(database_path)
 
 
 @pytest.fixture()
 def contact_entry():
+    """"""
     return ContactEntry("Vargas", "Angel", "08388764345", "12 court far, India")
 
 
 @pytest.mark.usefixtures("phone_book", "contact_entry")
-class TestPhoneBook:
+class TestPhoneBook(object):
     """
     Test units for the PhoneBook class
     """
+
+    def test_setup_database(self) -> None:
+        """
+        test table creation
+        :return:
+        """
+        database_connection = DB("test_database.sqlite3").connect()
+        database_connection.execute('''
+         CREATE TABLE IF NOT EXISTS phone_book (
+         id INTEGER PRIMARY KEY,
+         surname TEXT NOT NULL,
+         firstname TEXT NOT NULL,
+         phone_number TEXT NOT NULL,
+         address TEXT
+     );
+         ''')
+
+        database_connection.close()
 
     def test_create_phone_book_class_instance(self, phone_book) -> None:
         """
@@ -34,3 +64,44 @@ class TestPhoneBook:
         """
         _contact_entry = contact_entry
         assert isinstance(_contact_entry, ContactEntry)
+
+    def test_add_entry_to_phone_book(self, phone_book, contact_entry) -> None:
+        _result = phone_book.create(contact_entry)
+        assert _result == 1
+
+    def test_find_entry_in_phone_book(self, phone_book) -> None:
+        """
+
+        :param phone_book:
+        :return:
+        """
+        _result = phone_book.find(1)
+        assert _result == (1, 'Vargas', 'Angel', '08388764345', '12 court far, India')
+
+    def test_update_phone_book_entry(self, phone_book) -> None:
+        """
+
+        :param phone_book:
+        :return:
+        """
+        _result = phone_book.update(idx=1, firstname="Rufino")
+        _result = phone_book.find(1)
+        assert _result == (1, 'Vargas', 'Rufino', '08388764345', '12 court far, India')
+
+    def test_delete_entry_in_phone_book(self, phone_book) -> None:
+        """
+
+        :param phone_book:
+        :return:
+        """
+        _result = phone_book.delete("1")
+        assert _result == 1
+
+    def test_teardown_table(self) -> None:
+        """drops table created for tests"""
+        database_connection = DB("test_database.sqlite3").connect()
+        database_connection.execute('''
+         DROP TABLE IF EXISTS phone_book;
+         ''')
+
+        database_connection.close()
