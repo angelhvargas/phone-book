13a6e062cb1c11aa27a2835e95c149f2f2ad750e
Angel Vargas <angelvargas@outlook.es>
2019-09-30 00:17:08 +0100

    feat: added new api resources for the phone book service
    This new addition includes:
    - resource GET "/": simple message to let know to the client that's
    part of the resource.
    - resource GET "/contact/all" returns all the contacts in the given
    phone book service
    - resource GET "contact/<string:id>" returns a matching contact with the
    given id.

--- phonebook/phone_book_api.py
+++ phonebook/phone_book_api.py
@@ -1,25 +1,96 @@
 from flask import (
-    Blueprint, current_app as app, json, abort
+    Blueprint, current_app as app, Flask, abort, g
 )
-
+import json
 from phonebook.common.phonebook import PhoneBook
+from phonebook.db import get_db
 
 bp = Blueprint('phone_book_api', __name__)
 
 
-@bp.route('contact/all')
-def get_contact_all():
-    """"""
-    return api_response("hello", 200)
+@bp.route('/', methods=('GET',))
+def home():
+    _data = {"message": "phone book rest api, for api instructions navigate to: "}
+    return api_response(_data, 200)
+
+
+@bp.route('contact/all', methods=('GET',))
+def get_contact_all() -> Flask.make_response:
+    """
+    Return all the contacts in the
+    method: GET
+    resource: /contact/all
+    :return: JSON object |
+    {id: _id, surname: _surname, firstname: _firstname, phone_number: _phone_number, address: _address}
+    """
+    db = get_db()
+    pb = PhoneBook(db)
+    try:
+        _data = pb.all()
+
+        data = list()
+
+        for row in _data:
+            data.append(list(row))
+
+    except db.Error as e:
+        return api_response({'error': e}, 500)
+
+    return api_response(data, 200)
+
+
+@bp.route('contact', methods=("GET",))
+@bp.route('contact/<string:contact_id>', methods=("GET",))
+def get_contact(contact_id=-1) -> Flask.make_response:
+    """
+    :param contact_id: valid integer id to match for search
+    :return:
+    """
+    if contact_id == -1:
+        get_contact_all()
+
+    db = get_db()
+    pb = PhoneBook(db)
+    try:
+        _data = pb.find(contact_id)
+    except db.Error as e:
+        return api_response({'error': e}, 500)
+
+    return api_response(list(_data), 200)
+
+
+@bp.route('contact/search/<string:contact_id>', methods=("GET",))
+@bp.route('contact/<string:contact_id>/<string:attribute>', methods=("GET",))
+def search_contact(search: str, attribute=None) -> Flask.make_response:
+    """
+
+    returns contact matching exactly a given value in a given attribute,
+    by default this resource will use the contact
+    id, this means is different than the search resource.
+
+    :param search:
+    :param attribute:
+    :return:
+    """
 
 
 @bp.route('<path:path>', methods=('GET', 'POST', 'PUT', 'DELETE',))
 def missing_request(path):
-    """"""
+    """
+    Handles any missing request which is not part of the API.
+    :param path:
+    :return:
+    """
     abort(400, {'message': 'Invalid request'})
 
 
 def api_response(data_, status_):
+    """
+    Prepares the API response.
+    :param data_:
+    :param status_:
+    :return:
+    """
     response_ = app.response_class(
         response=json.dumps(data_),
         status=status_,
