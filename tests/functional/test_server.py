49eb8110736bf956aac7048e5d511455592168c3
Angel Vargas <angelvargas@outlook.es>
2019-10-02 23:56:02 +0100

    test: Added new test to create new contacts
    Phone book contacts coverage have been improved, using correct assertion

--- tests/functional/test_server.py
+++ tests/functional/test_server.py
@@ -1,6 +1,5 @@
 import pytest
 import flask
-import json
 
 
 @pytest.mark.usefixtures('app', 'client', 'runner')
@@ -8,14 +7,15 @@ class TestServer:
     """
     Unit Tests for phone book api blueprint.
     """
-    def test_fetch_all_contacts(self, client: flask.Flask) -> None:
+
+    def test_get_all_contacts(self, client: flask.Flask) -> None:
         """
-        Test resource GET -> /contact/all ¬ returns -> JSON object with all the contacts in the phone book
+        Test resource GET -> /contacts ¬ returns -> JSON object with all the contacts in the phone book
         :param client:
         :return:
         """
         with client as c:
-            resp = c.get('/contact/all')
+            resp = c.get('/contacts')
             data = flask.json.loads(resp.data)
             assert data == flask.json.loads(
                 '[[4, "Dijkstra", "Edsger", "3581321345589", "19-68 Algo"], [3, "Noam", "Chomsky", "3581321345589", '
@@ -23,21 +23,44 @@ class TestServer:
                 '"Dennis", "1235813213455", "19-41 C block"]]'
             )
 
+    @pytest.mark.parametrize("surname, firstname, phone_number, address, expected",
+                             [("Vargas","Angel", "27364523", "somewhere",
+                               '{"address": "somewhere","firstname": "Angel","id": 5, '
+                               '"phone_number": "27364523", "surname": "Vargas"}')])
+    def test_create_new_contact(self, surname, firstname, phone_number, address, expected, client: flask.Flask) -> None:
+        """
+        Test resource POST:/contacts does create a new entry in the phonebook
+        :param surname:
+        :param firstname:
+        :param phone_number:
+        :param address:
+        :param expected:
+        :param client:
+        :return:
+        """
+        with client as c:
+            data = dict(surname=surname, firstname=firstname, phone_number=phone_number, address=address)
+            resp = c.post('/contacts', data=data, follow_redirects=True)
+            data = flask.json.loads(resp.data)
+        assert data == flask.json.loads(expected)
+
     @pytest.mark.parametrize("_id,expected", [
         ("3", '[3, "Noam", "Chomsky", "3581321345589", "19-56 R block"]'),
         ("2", '[2, "Stroustrup", "Bjarne", "3581321345589", "19-79 C block"]'),
         ("1", '[1, "Ritchie", "Dennis", "1235813213455", "19-41 C block"]')
 
     ])
-    def test_find_a_contact(self, _id, expected, client: flask.Flask) -> None:
+    def test_get_contact_by_id(self, _id, expected, client: flask.Flask) -> None:
         """
         Test resource GET -> /contact/[id] ¬ returns -> JSON object with a matching contact found in the phone book
+        :param _id:
+        :param expected:
         :param client:
         :return:
         """
 
         with client as c:
-            resp = c.get('/contact/{}'.format(_id))
+            resp = c.get('/contacts/{}'.format(_id))
             data = flask.json.loads(resp.data)
             assert data == flask.json.loads(
                 expected
